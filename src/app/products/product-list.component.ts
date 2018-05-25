ac6573c5e1edc73682ff051653a032b2415b1e0d
Pratima <pagodse@gmail.com>
2018-05-25 15:35:42 -0500

    V1.0.0

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -83,4 +83,8 @@ export class ProductListComponent implements OnInit{
         return this.products.filter((product: IProduct) =>
                 product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
         }
+
+        onRatingClicked(message:string) :void{
+            this.pageTitle = "Product List : " + message ;
+        }
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from "@angular/core";
+import { Component, OnChanges, Input, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -11,6 +11,12 @@ export class StartComponent implements OnChanges{
     ngOnChanges(): void {
         this.starWidth = this.rating * 86/5;
     }
+    onClick(): void {
+        console.log(`Passed nested Child event to parent container : The Ratig ${this.rating} was clicked!`)
+        this.ratingClicked.emit(`The Ratig ${this.rating} was clicked!`)
+    }
     @Input() rating : number;
     starWidth : number;    
+    @Output() ratingClicked : EventEmitter<string> =
+    new EventEmitter<string>();
 }
