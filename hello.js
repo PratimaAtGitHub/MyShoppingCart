5942f841473aa912e0509c5b4a8895f011b2bd7b
Pratima <pagodse@gmail.com>
2018-05-29 23:06:11 -0500

    29May18

---
+++ hello.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+    res.end('<br> Hello Sanika G!!');
+}).listen(7080);
+
+console.log("Heloo Sanika!");
